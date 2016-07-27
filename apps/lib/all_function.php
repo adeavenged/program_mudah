1cbecab2d193d1854eefb13d5ded623afb9d3c5e
root <dheendra7393@gmail.com>
2016-07-27 11:26:25 +0700

    PM_1.0

--- apps/lib/all_function.php
+++ apps/lib/all_function.php
@@ -3,11 +3,18 @@
 
 	function nama_m($id)
 	{
-		$sql = mysql_query("SELECT * FROM menu WHERE id_menu = $id") or die(mysql_error());
+		$sql = mysql_query("SELECT * FROM menu WHERE id_menu = '$id'") or die(mysql_error());
 		$m = mysql_fetch_assoc($sql);
 
 		return $m['nama_menu'];
 	}
+
+	function anti_inject($data)
+	{
+		$filter_sql = stripslashes(strip_tags(htmlspecialchars($data,ENT_QUOTES)));
+		return $filter_sql;
+	}
+
 
 
 ?>
